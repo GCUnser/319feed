57ace95a2557143e78bc254de167d2c4d0357a10
Muralikrishna Patibandla <muralip@iastate.edu>
2024-03-09 22:57:33 -0600

    quiz title issue

--- quiz.js
+++ quiz.js
@@ -1,16 +1,21 @@
 fetch("./data.json")
 .then(response => response.json())
-.then(myQuizzes => loadQuiz(myQuizzes.quizzes[localStorage.getItem('selectedQuizType')]));
+.then(myQuizzes => {
+    const selectedQuiz = myQuizzes.quizzes[localStorage.getItem('selectedQuizType')];
+    loadQuiz(selectedQuiz);
+});
 
 function loadQuiz(quiz) {
-    var CardTitle = document.getElementById("quizTitle");
+    const quizTitleDiv = document.getElementById("quizTitle");
+    const container = document.getElementById("quizContainer");
 
-    let quizTitle = document.createElement("h1");
+    quizTitleDiv.innerHTML = '';
+    container.innerHTML = '';
+
+    const quizTitle = document.createElement("h1");
     quizTitle.classList.add("quiz-heading");
     quizTitle.innerText = quiz.title;
-    CardTitle.appendChild(quizTitle);
-
-    const container = document.querySelector('.container');
+    quizTitleDiv.appendChild(quizTitle);
 
     Object.keys(quiz).forEach(key => {
         if(key.startsWith("question")) {
@@ -31,13 +36,13 @@ function loadQuiz(quiz) {
                 answerDiv.classList.add("answer");
                 
                 const answerInput = document.createElement("input");
-                answerInput.setAttribute("type", "radio");
-                answerInput.setAttribute("name", key);
-                answerInput.setAttribute("id", answer);
-                answerInput.setAttribute("value", answer);
+                answerInput.type = "radio";
+                answerInput.name = key;
+                answerInput.id = answer;
+                answerInput.value = answer;
 
                 const answerLabel = document.createElement("label");
-                answerLabel.setAttribute("for", answer);
+                answerLabel.htmlFor = answer;
                 answerLabel.innerText = answer;
 
                 answerDiv.appendChild(answerInput);
@@ -53,36 +58,8 @@ function loadQuiz(quiz) {
     const submitButton = document.createElement("button");
     submitButton.classList.add("btn", "btn-primary", "submit-button");
     submitButton.innerText = "Submit";
-    container.appendChild(submitButton);
-
     submitButton.addEventListener('click', function() {
         console.log("Congratulations! Your responses have been successfully submitted!");
     });
+    container.appendChild(submitButton);
 }
-
-
-
-/*fetch("./data.json")
-.then(response => response.json())
-.then(myQuizzes => loadQuizzes(myQuizzes));
-
-function loadQuizzes(myQuizzes) {
-
-    var CardTitle = document.getElementById("quizTitle")
-
-    let title = myQuizzes.quizzes[localStorage.getItem('selectedQuizType')].title;
-
-    let AddCardTitle = document.createElement("h1");
-    AddCardTitle.classList.add("quiz-heading");
-    AddCardTitle.innerHTML=`
-        <span class="overline"></span>
-        ${title}
-        <span class="underline"></span>
-    `
-    CardTitle.appendChild(AddCardTitle);
-
-   
-} // end of function
-
-
-*/
