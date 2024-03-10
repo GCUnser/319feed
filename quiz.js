bfd35a0e12ec772cfce99d916b24d4ac33ef3a01
Muralikrishna Patibandla <muralip@iastate.edu>
2024-03-09 22:44:13 -0600

    quiz logic and styling

--- quiz.js
+++ quiz.js
@@ -1,5 +1,69 @@
 fetch("./data.json")
 .then(response => response.json())
+.then(myQuizzes => loadQuiz(myQuizzes.quizzes[localStorage.getItem('selectedQuizType')]));
+
+function loadQuiz(quiz) {
+    var CardTitle = document.getElementById("quizTitle");
+
+    let quizTitle = document.createElement("h1");
+    quizTitle.classList.add("quiz-heading");
+    quizTitle.innerText = quiz.title;
+    CardTitle.appendChild(quizTitle);
+
+    const container = document.querySelector('.container');
+
+    Object.keys(quiz).forEach(key => {
+        if(key.startsWith("question")) {
+            const questionObj = quiz[key];
+            const questionContainer = document.createElement("div");
+            questionContainer.classList.add("question-container");
+
+            const questionTitle = document.createElement("h2");
+            questionTitle.classList.add("question");
+            questionTitle.innerText = questionObj.question;
+            questionContainer.appendChild(questionTitle);
+
+            const answersDiv = document.createElement("div");
+            answersDiv.classList.add("answers");
+
+            questionObj.answers.forEach(answer => {
+                const answerDiv = document.createElement("div");
+                answerDiv.classList.add("answer");
+                
+                const answerInput = document.createElement("input");
+                answerInput.setAttribute("type", "radio");
+                answerInput.setAttribute("name", key);
+                answerInput.setAttribute("id", answer);
+                answerInput.setAttribute("value", answer);
+
+                const answerLabel = document.createElement("label");
+                answerLabel.setAttribute("for", answer);
+                answerLabel.innerText = answer;
+
+                answerDiv.appendChild(answerInput);
+                answerDiv.appendChild(answerLabel);
+                answersDiv.appendChild(answerDiv);
+            });
+
+            questionContainer.appendChild(answersDiv);
+            container.appendChild(questionContainer);
+        }
+    });
+
+    const submitButton = document.createElement("button");
+    submitButton.classList.add("btn", "btn-primary", "submit-button");
+    submitButton.innerText = "Submit";
+    container.appendChild(submitButton);
+
+    submitButton.addEventListener('click', function() {
+        console.log("Congratulations! Your responses have been successfully submitted!");
+    });
+}
+
+
+
+/*fetch("./data.json")
+.then(response => response.json())
 .then(myQuizzes => loadQuizzes(myQuizzes));
 
 function loadQuizzes(myQuizzes) {
@@ -21,3 +85,4 @@ function loadQuizzes(myQuizzes) {
 } // end of function
 
 
+*/
