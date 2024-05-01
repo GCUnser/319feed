d171302a96c7c70c62d4a2bc81c6f76dee620b85
Gabriel Unser <gcunser@gmail.com>
2024-04-30 21:57:22 -0500

    renaming some database stuff - database "319feed" collection "quizzes"

--- backend/index.js
+++ backend/index.js
@@ -12,7 +12,7 @@ var bodyParser = require("body-parser");
 
 // MongoDB
 const url = "mongodb://127.0.0.1:27017";
-const dbName = "reactdata";
+const dbName = "319feed";
 const client = new MongoClient(url);
 const db = client.db(dbName);
 
@@ -21,18 +21,18 @@ const host = "localhost";
 app.use(cors());
 app.use(express.json());
 
-app.listen(PORT, () => {
+/* app.listen(PORT, () => {
   console.log("App listening at http://%s:%s", host, PORT);
 });
 
-/* app.get("/catalog", async (req, res) => {
+app.get("/catalog", async (req, res) => {
   try {
     await client.connect();
     console.log("Node connected successfully to GET MongoDB");
     const query = {};
-    const result = await db.collection("fakestore_catalog").find(query).limit(100).toArray(); 
+    const result = await db.collection("quizzes").find(query).limit(100).toArray();
     console.log("Success in Reading MongoDB");
-    res.status(200).send(result); 
+    res.status(200).send(result);
   } catch (err) {
     console.error("Error in Reading MongoDB :", err);
     res.status(500).send({ error: "An error occurred while fetching items." });
@@ -42,20 +42,20 @@ app.listen(PORT, () => {
 app.get("/catalog/:id", async (req, res) => {
   try {
     const id = Number(req.params.id);
-    console.log("Product to find :", id);
+    console.log("Quiz to find :", id);
     await client.connect();
     console.log("Node connected successfully to GET-id MongoDB");
     const query = { id: id };
 
-  const results = await db.collection("fakestore_catalog").findOne(query);
+    const results = await db.collection("quizzes").findOne(query);
 
-  if (!results) {
-    res.status(404).send("Not Found");
-  } else {
-    res.status(200).send(results);
-  }
+    if (!results) {
+      res.status(404).send("Not Found");
+    } else {
+      res.status(200).send(results);
+    }
 
-  console.log("Results :", results);
+    console.log("Results :", results);
   } catch (err) {
     console.error("Error in Reading MongoDB :", err);
     if (!res.headersSent) {
@@ -73,17 +73,16 @@ app.post("/catalog", async (req, res) => {
       console.log(msg);
       return res.status(400).send({ error: msg });
     }
-    
+
     const itemId = req.body.id;
-    query = { id: itemId }
-    const productExists = await db.collection("fakestore_catalog").findOne(query);
-    if (productExists) {
+    query = { id: itemId };
+    const quizExists = await db.collection("quizzes").findOne(query);
+    if (quizExists) {
       const msg = "POST:Item with this ID already exists";
       console.log(msg);
       return res.status(409).send({ error: msg });
     }
-    
-    
+
     const keys = Object.keys(req.body);
     const values = Object.values(req.body);
     const newDocument = {
@@ -95,18 +94,17 @@ app.post("/catalog", async (req, res) => {
       image: values[5],
       rating: {
         rate: Number(values[6].rate),
-        count: Number(values[6].count)
-      }
+        count: Number(values[6].count),
+      },
     };
     console.log(newDocument);
 
-    const results = await db.collection("fakestore_catalog").insertOne(newDocument);
+    const results = await db.collection("quizzes").insertOne(newDocument);
 
     const msg = "POST:Success in Posting";
     console.log(msg);
     return res.status(200).send({ success: msg });
   } catch (err) {
-    
     const msg = "POST: An ERROR occurred in Post" + err;
     console.error(msg);
     res.status(500).send({ error: msg });
@@ -114,63 +112,61 @@ app.post("/catalog", async (req, res) => {
 });
 
 app.put("/catalog/:id", async (req, res) => {
-    const id = Number(req.params.id);
+  const id = Number(req.params.id);
 
-    await client.connect();
-    const query = { id: id };
-    console.log("Product to Update :", id);
-    const productToUpdate = await db.collection("fakestore_catalog").findOne(query);
-    if (!productToUpdate) {
-      res.status(404).send({ message: "Product not found" });
-      return;
-    }
-    
+  await client.connect();
+  const query = { id: id };
+  console.log("Quiz to Update :", id);
+  const quizToUpdate = await db.collection("quizzes").findOne(query);
+  if (!quizToUpdate) {
+    res.status(404).send({ message: "Quiz not found" });
+    return;
+  }
 
-    const updateData = {
-      $set: {
-        ...req.body.title && { title: req.body.title },
-        ...req.body.price && { price: Number(req.body.price) },
-        ...req.body.description && { description: req.body.description },
-        ...req.body.category && { category: req.body.category },
-        ...req.body.image && { image: req.body.image },
-        ...(req.body.rating && req.body.rating.rate && { 'rating.rate': Number(req.body.rating.rate) }),
-        ...(req.body.rating && req.body.rating.count && { 'rating.count': Number(req.body.rating.count) }),
-      }
-    };
+  const updateData = {
+    $set: {
+      ...(req.body.title && { title: req.body.title }),
+      ...(req.body.price && { price: Number(req.body.price) }),
+      ...(req.body.description && { description: req.body.description }),
+      ...(req.body.category && { category: req.body.category }),
+      ...(req.body.image && { image: req.body.image }),
+      ...(req.body.rating && req.body.rating.rate && { "rating.rate": Number(req.body.rating.rate) }),
+      ...(req.body.rating && req.body.rating.count && { "rating.count": Number(req.body.rating.count) }),
+    },
+  };
 
-    const results = await db.collection("fakestore_catalog").updateOne(query, updateData)
-    if (results.matchedCount === 0) {
-      res.status(404).send({ message: "Product not found" });
-      return;
-    }
-      const msg = "PUT:Success in updating product";
-      console.log(msg);
-      return res.status(200).json({ success: msg, product: productToUpdate });
-    
-  });
+  const results = await db.collection("quizzes").updateOne(query, updateData);
+  if (results.matchedCount === 0) {
+    res.status(404).send({ message: "Quiz not found" });
+    return;
+  }
+  const msg = "PUT:Success in updating quiz";
+  console.log(msg);
+  return res.status(200).json({ success: msg, quiz: quizToUpdate });
+});
 
 app.delete("/catalog/:id", async (req, res) => {
   try {
     const itemId = Number(req.params.id);
     await client.connect();
-    console.log("Product to delete :", itemId);
+    console.log("Quiz to delete :", itemId);
 
     const query = { id: itemId };
 
-    const productToDelete = await db.collection("fakestore_catalog").findOne(query);
-      if (!productToDelete) {
-        res.status(404).send({ message: "Product not found" });
-        return;
-      }
+    const quizToDelete = await db.collection("quizzes").findOne(query);
+    if (!quizToDelete) {
+      res.status(404).send({ message: "Quiz not found" });
+      return;
+    }
 
-      const deletionResult = await db.collection("fakestore_catalog").deleteOne(query);
-      if (deletionResult.deletedCount === 0) {
-        throw new Error('Error during deletion');
-      }
+    const deletionResult = await db.collection("quizzes").deleteOne(query);
+    if (deletionResult.deletedCount === 0) {
+      throw new Error("Error during deletion");
+    }
 
-    const msg = "Success in DELETE product";
+    const msg = "Success in DELETE quiz";
     console.log(msg);
-    return res.status(200).send(productToDelete);
+    return res.status(200).send(quizToDelete);
   } catch (err) {
     const msg = "DELETE: An ERROR occurred in Delete" + err;
     console.error(msg);
